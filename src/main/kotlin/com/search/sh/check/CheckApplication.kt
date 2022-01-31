33392cf8834ed171e10c9c9ba101f52eedeac501
lydia.s.kang@nujackpot.com-at-831384894519 <cv8GZQzyzGrVIMmyVtW6FnUQjwzTckbJgQT3nz2NSTg=>
2022-01-31 17:11:25 +0900

    init

---
+++ src/main/kotlin/com/search/sh/check/CheckApplication.kt
@@ -0,0 +1,11 @@
+package com.search.sh.check
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CheckApplication
+
+fun main(args: Array<String>) {
+	runApplication<CheckApplication>(*args)
+}
